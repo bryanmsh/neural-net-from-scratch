0a2dc40f886f6cc1ea70a650bf0226fc07d0a0c9
bryanmsh <bryanmsh@gmail.com>
2026-08-15 15:15:01 -0400

    added main file and some code to test

---
+++ main.py
@@ -0,0 +1,25 @@
+import network as nn
+import data_loader as dl
+import numpy as np
+
+if __name__ == "__main__":
+    training_labels = dl.load_labels("./MNIST/train-labels-idx1-ubyte")
+    training_images = dl.load_img("./MNIST/train-images-idx3-ubyte")
+
+    # test_labels = dl.load_labels("./MNIST/t10k-labels-idx1-ubyte")
+    # test_images = dl.load_img("./MNIST/t10k-images-idx3-ubyte")
+
+    training_images = training_images / 255.0
+    training_labels = dl.one_hot(training_labels)
+
+    network = nn.NeuralNetwork([784,16,16,10]) # create neural net
+    output = network.forward(training_images[0]) # forward this row (this image)
+
+    print(output)
+    print(training_labels[0]) 
+
+
+    for image, label in zip(training_images, training_labels): 
+        # iterate through image and label at the same time, so the label is attached to the image
+        output = network.forward(image)
+        
