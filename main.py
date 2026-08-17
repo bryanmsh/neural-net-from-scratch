2960d55606bb5a692154aaf1365981252d912c9e
bryanmsh <bryanmsh@gmail.com>
2026-08-17 15:34:27 -0400

    added cost and avg_cost calculation functions

--- main.py
+++ main.py
@@ -6,11 +6,12 @@ if __name__ == "__main__":
     training_labels = dl.load_labels("./MNIST/train-labels-idx1-ubyte")
     training_images = dl.load_img("./MNIST/train-images-idx3-ubyte")
 
+    training_images = training_images / 255.0
+    training_labels = dl.one_hot(training_labels)
+
     # test_labels = dl.load_labels("./MNIST/t10k-labels-idx1-ubyte")
     # test_images = dl.load_img("./MNIST/t10k-images-idx3-ubyte")
 
-    training_images = training_images / 255.0
-    training_labels = dl.one_hot(training_labels)
 
     network = nn.NeuralNetwork([784,16,16,10]) # create neural net
     output = network.forward(training_images[0]) # forward this row (this image)
--- train.py
+++ train.py
@@ -1,13 +1,31 @@
 import numpy as np
+import network as nn
+import data_loader as dl
 
-a = np.array([1, 0, 1, 1, 0])
-b = np.array([0, 1, 0, 0, 0])
 
-print(a, b)
-print((a-b)**2)
+def cost(output, label): # cost for 1 output
+    return np.sum((output-label)**2)
 
-sum = 0
-for i in (a-b)**2:
-    sum+=i
+def avg_cost(img_set, label_set):
+    total = 0
+    for image, label in zip(img_set, label_set): 
+        total += cost(network.forward(image), label) 
+        # sum cost of each output
+    total /= len(label_set) # divide for avg
+    return (total)
 
-print(sum)
+
+if __name__ == "__main__":
+    training_labels = dl.load_labels("./MNIST/train-labels-idx1-ubyte")
+    training_images = dl.load_img("./MNIST/train-images-idx3-ubyte")
+
+    training_images = training_images / 255.0
+    training_labels = dl.one_hot(training_labels)
+
+    # output = np.array([0,0,0])
+    # label = np.array([0,1,0])
+    # print((output-label)**2)
+    network = nn.NeuralNetwork([784,16,16,10])
+    print(cost(network.forward(training_images[0]), training_labels[0]))
+    print(avg_cost(training_images, training_labels))
+
