8e9a33065e5dcfef8abc94b4c9088f611979683a
bryanmsh <bryanmsh@gmail.com>
2026-08-26 15:41:07 -0400

    updates params after each image

--- train.py
+++ train.py
@@ -16,32 +16,24 @@ def avg_cost(network, img_set, label_set):
     return (total)
 
 def train(network, img_set, label_set, learning_rate, num_epochs=7):
-    load_params(network)
+    #load_params(network)
+    print(f"Initial average cost: {avg_cost(network, img_set, label_set)}")
+    
     for epoch in range(1, num_epochs+1):
-
-        total_dcdB = [np.zeros_like(layer.B) for layer in network.layers]
-        total_dcdW = [np.zeros_like(layer.W) for layer in network.layers]
-        # initialize totals, format same as the arrays they sum
 
         for image, label in zip(img_set, label_set):
             output = network.forward(image)
             network.backward(output, label)
 
-            for i, layer in enumerate(network.layers):
-                total_dcdB[i] += layer.dcdB
-                total_dcdW[i] += layer.dcdW
-                # cycle through each layer in network and sum gradients
-                
-        count = len(img_set)
-        for i, layer in enumerate(network.layers):
-            # need to cycle through layers again
-            layer.W -= learning_rate * (total_dcdW[i]/count)
-            layer.B -= learning_rate * (total_dcdB[i]/count)
-        
+            for layer in network.layers:
+                layer.W -= learning_rate * layer.dcdW
+                layer.B -= learning_rate * layer.dcdB
+                # cycle through each layer in network and update params
             
-        print(f"Total cost for epoch {epoch}: {avg_cost(network, img_set, label_set)}")
+            
+        print(f"Average cost for epoch {epoch}: {avg_cost(network, img_set, label_set)}")
 
-    save_params(network)
+    #save_params(network)
         
 
 if __name__ == "__main__":
@@ -55,8 +47,8 @@ if __name__ == "__main__":
     # label = np.array([0,1,0])
     # print((output-label)**2)
     network = nn.NeuralNetwork([784,16,16,10])
-    load_params(network)
+    
     # print(cost(network.forward(training_images[0]), training_labels[0]))
     # print(avg_cost(network, training_images, training_labels))
 
-    train(network, training_images, training_labels, 1, 15)
+    train(network, training_images, training_labels, .05, 5)
